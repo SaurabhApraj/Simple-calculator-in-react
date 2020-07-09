5a3e34d60f28f403eebb2a2f269be79619dfa11e
SaurabhApraj <saurabhapraj7@gmail.com>
2020-07-09 13:36:13 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { add, sub, div, multi } from "./Calc";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ul>
+        <li>Addition is : {add(40, 4)}</li>
+        <li>Subtraction : {sub(40, 4)}</li>
+        <li>Division is : {div(40, 4)}</li>
+        <li>Multiplication is : {multi(40, 4)}</li>
+      </ul>
+    </>
   );
 }
 
---
+++ src/Calc.jsx
@@ -0,0 +1,19 @@
+function add(a, b) {
+  let sum = a + b;
+  return sum;
+}
+function sub(a, b) {
+  let sub = a - b;
+  return sub;
+}
+function div(a, b) {
+  let div = a / b;
+  div = div.toFixed(2);
+  return div;
+}
+function multi(a, b) {
+  let mult = a * b;
+  return mult;
+}
+
+export { add, sub, div, multi };
